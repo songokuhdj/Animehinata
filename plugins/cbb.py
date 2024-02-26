c61e863c071acd3a3265e0a98ac70d5c1d6d85fe
songokuop <149889315+songokuop@users.noreply.github.com>
2024-02-26 09:50:46 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,12 +10,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "help":
         await query.message.edit_text(
-            text = f"<b>Bot Cammands\n❏ Commands For BOT Admins\n├/start : start the bot or get posts\n├/batch : Create Group Message\n├/genlink : create link for one post\n├/users : view bot statistics\n├/broadcast : broadcast Message\n└/stats : checking your bot uptime\n\n👨‍💻 Developed by <a href=https://t.me/Its_Oreki_Hotarou>Hōᴛᴀʀō Oʀᴇᴋɪ</a></b>",
+            text = f"<b>𝖡𝖮𝖳 𝖢𝗈𝗆𝗆𝖺𝗇𝖽𝗌\n❏ 𝖢𝗈𝗆𝗆𝖺𝗇𝖽𝗌 𝖥𝗈𝗋 𝖡𝖮𝖳 𝖠𝖽𝗆𝗂𝗇𝗌\n├/start : start the bot or get posts\n├/batch : Create Group Message\n├/genlink : create link for one post\n├/users : view bot statistics\n├/broadcast : broadcast Message\n└/stats : checking your bot uptime\n\n👨‍💻 𝖣𝖾𝗏𝖾𝗅𝗈𝗉𝖾𝖽 𝖻𝗒 <a href=https://t.me/AnimeDiversity>𝖠𝗇𝗂𝗆𝖾 𝖣𝗂𝗏𝖾𝗋𝗌𝗂𝗍𝗒</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("💥​ᴄʟᴏꜱᴇ​💥", callback_data="close"),
+                        InlineKeyboardButton("💥ᴄʟᴏꜱᴇ💥", callback_data="close"),
                         InlineKeyboardButton("⚡ᴀʙᴏᴜᴛ⚡", callback_data="about")
                     ]
                 ]
@@ -23,12 +23,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         )
     elif data == "about":
         await query.message.edit_text(
-            text = f"<b>Hi there this is a file store bot which is convert any file to link...\nthen you can access this file through a specific link...!\n\nCreator - @Its_Oreki_Hotarou\nMy Channel - @Anime_X_Hunters\nDicsussion Group - @Hunters_Discussion</a>\n\n👨‍💻 Developed by @Its_Oreki_Hotarou</b>",
+            text = f"<b>Hi there this is a file store bot which is convert any file to link...\nthen you can access this file through a specific link...!\n\nMy Channel <a href=https://t.me/AnimeDiversity>𝖠𝗇𝗂𝗆𝖾 𝖣𝗂𝗏𝖾𝗋𝗌𝗂𝗍𝗒</a>\n\n👨‍💻 𝖣𝖾𝗏𝖾𝗅𝗈𝗉𝖾𝖽 𝖻𝗒 <a href=https://t.me/AnimeDiversity>𝖠𝗇𝗂𝗆𝖾 𝖣𝗂𝗏𝖾𝗋𝗌𝗂𝗍𝗒</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("💥​ᴄʟᴏꜱᴇ​💥", callback_data="close"),
+                        InlineKeyboardButton("💥ᴄʟᴏꜱᴇ💥", callback_data="close"),
                         InlineKeyboardButton("🚀ʜᴇʟᴘ🚀", callback_data="help")
                     ]
                 ]
