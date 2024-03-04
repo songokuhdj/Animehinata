5572726c4bcfd7414f168fef8868f7214d9397bf
songokuhdj <157366736+songokuhdj@users.noreply.github.com>
2024-03-04 18:26:30 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,11 +8,8 @@ from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 from bot import Bot
 from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
 from helper_func import subscribed, encode, decode, get_messages
-from database.database import add_user, del_user, full_userbase, present_user
 
-"""add time im seconds for waitingwaiting before delete 
-1min=60, 2min=60×2=120, 5min=60×5=300"""
-SECONDS = int(os.getenv("SECONDS", "600"))
+
 
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
@@ -60,7 +57,6 @@ async def start_command(client: Client, message: Message):
             return
         await temp_msg.delete()
 
-        Ujjwal = []
         for msg in messages:
 
             if bool(CUSTOM_CAPTION) & bool(msg.document):
@@ -74,25 +70,13 @@ async def start_command(client: Client, message: Message):
                 reply_markup = None
 
             try:
-                uj = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
+                await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
                 await asyncio.sleep(0.5)
-                Ujjwal.append(uj)
             except FloodWait as e:
                 await asyncio.sleep(e.x)
-                uj = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
-                Ujjwal.append(uj)
+                await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
             except:
                 pass
-
-        await message.reply_text("**Files will be deleted in 10 minutes.**\n__Forward to saved messages before downloading__")
-        await asyncio.sleep(SECONDS)
-
-        for data in Ujjwal:
-            try:
-                await data.delete()
-            except:
-                pass
-
         return
     else:
         reply_markup = InlineKeyboardMarkup(
